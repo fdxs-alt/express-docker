7663c8b0ae825e2c938be34a55e3bc8204a1ac86
Jakub Sukiennik <kuba1207710@gmail.com>
2020-12-30 08:27:34 +0100

    changing to snowpack

---
+++ frontend/snowpack.config.js
@@ -0,0 +1,26 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/dist",
+  },
+  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-dotenv"],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
